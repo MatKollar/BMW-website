4d3e172fc4950ad86c38ae3751f85b5d4adc5553
MatKollar <matej.kollar3@gmail.com>
2022-06-04 17:22:25 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let cursorInner = document.querySelector('.cursor-inner');
+let cursorOuter = document.querySelector('.cursor-outer');
+
+window.addEventListener("mousemove", cursorMoving)
+
+function cursorMoving(e) {
+    cursorInner.style.top = e.clientY + "px"
+    cursorInner.style.left = e.clientX + "px"
+
+    cursorOuter.style.top = e.clientY + "px"
+    cursorOuter.style.left = e.clientX + "px"
+
+}
